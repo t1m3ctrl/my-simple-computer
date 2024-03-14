419fb5d69fb24302da05fa23a4fd3237cc94f34e
Сотнич Алдар Святославович <ip211s25@csc.sibsutis.ru>
2024-03-14 10:58:44 +0700

    added header file

--- include/mt.h
+++ include/mt.h
@@ -1,19 +1,22 @@
-#ifndef SC_H
-#define SC_H
+#ifndef MT_H
+#define MT_H
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
+#include <termios.h>
 #include <unistd.h>
 
-enum colors {
-    BLACK,
-    RED,
-    GREEN,
-    YELLOW,
-    BLUE,
-    MAGENTA,
-    CYAN,
-    WHITE
+enum colors
+{
+  BLACK,
+  RED,
+  GREEN,
+  YELLOW,
+  BLUE,
+  MAGENTA,
+  CYAN,
+  WHITE
 };
 
 int mt_clrscr (void);
@@ -24,6 +27,5 @@ int mt_setbgcolor (enum colors color);
 int mt_setdefaultcolor (void);
 int mt_setcursorvisible (int value);
 int mt_delline (void);
-
 
 #endif
